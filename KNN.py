e5b968839c5cd88e2a25195a962d1281492301ec
Joshua Tobin <69868231+tobinjo96@users.noreply.github.com>
2021-05-12 17:23:11 +0100

    Update to KNN.py
    Improvements in computation method and run time.

--- KNN.py
+++ KNN.py
@@ -1,35 +1,44 @@
 import numpy as np
+from tqdm import tqdm
+import multiprocessing as mp
+import sharedmem
+import itertools
+import matplotlib.pyplot as plt
+import scipy.spatial.distance as distance
+import scipy.stats
+import aghasher
+import math
 import sys
 import os
-import aghasher
-import scipy.sparse
-import math
-import multiprocessing as mp
-import itertools
-#import CPFcluster.utils as utils
-import utils
+#from sklearn.metrics.pairwise import cosine_distances
+from sklearn.metrics.pairwise import euclidean_distances
 
 def get_knn(X, k, reps = 50, num_hashbits = 12, blocksz = 100, n_core = 1):
+  X = np.array(X).astype(float)
+  edge_arr = np.empty(shape = (X.shape[0], k))
+  edge_arr[:] = np.nan
+  w_arr = np.empty(shape = (X.shape[0], k))
+  w_arr[:] = np.nan
   if X.shape[0] < 1000:
-    edge_arr = np.empty((X.shape[0], k))
-    edge_arr[:] = np.nan
-    w_arr = np.ones((X.shape[0], k))*np.inf
     PDmat = distance_matrix(X)
-    for i in range(len(PDmat)):
-      w_arr[i, 0:k] = np.sort(PDmat[i,:])[1:k+1]
-      edge_arr[i, 0:k] = np.argsort(PDmat[i,:])[1:k+1]
-    
+    w_arr = np.array(list(map(np.sort, PDmat)))[:, range(k)]
+    edge_arr = np.array(list(map(np.argsort, PDmat))).astype(int)[:,range(k)]
     return edge_arr, w_arr
-  else: 
-    edge_arr = np.empty(shape = (X.shape[0], k))
-    edge_arr[:] = np.nan
-    w_arr = np.ones((X.shape[0], k))*np.inf
-    for rep in range(reps):
+  else:
+    nrep = 25
+    for rep in tqdm(range(nrep), "Computing Nearest Neighbors:"):
       edge_arr, w_arr = basic_ann_lsh(X, k, num_hashbits, blocksz, n_core, edge_arr, w_arr)
+    
+    narows = [x for x in range(edge_arr.shape[0]) if np.inf in edge_arr[x, :]]
+    for x in narows:
+      na_dists = np.sqrt(np.square(X[x,:] - X).sum(1))
+      w_arr[x,:] = np.sort(na_dists)[range(1, edge_arr.shape[1] + 1)]
+      edge_arr[x, :] = np.argsort(na_dists)[range(1, edge_arr.shape[1] + 1)]
     
     edge_arr, w_arr = refine(X, edge_arr, w_arr)
     return edge_arr, w_arr
 
+#Functions Used
 def basic_ann_lsh(X, k, m, blocksz, n_core, edge_arr, w_arr):
   agh = None
   while agh is None:
@@ -38,7 +47,12 @@ def basic_ann_lsh(X, k, m, blocksz, n_core, edge_arr, w_arr):
       anchor = X[anchoridx, :]
       agh, H_train = aghasher.AnchorGraphHasher.train(X, anchor, m)
     except:
-      pass
+      try:
+        anchoridx = np.random.choice(X.shape[0], size = 25, replace = False)
+        anchor = X[anchoridx, :]
+        agh, H_train = aghasher.AnchorGraphHasher.train(X, anchor, m)
+      except: 
+        pass
   
   Y = agh.hash(X)
   Y = Y.astype(int)
@@ -49,6 +63,7 @@ def basic_ann_lsh(X, k, m, blocksz, n_core, edge_arr, w_arr):
   l = ([X[block,: ], block, k, blocksz, edge_arr[block,:], w_arr[block,:]] for block in list(chunks(pid, blocksz)))
   try:
     pool = mp.Pool(processes=n_core)              
+    #result = [[]]*math.ceil(len(list(chunks(pid, blocksz)))/n_core)
     result = []
     N = max(math.floor(X.shape[0]/(100*blocksz)), 20)
     while True:
@@ -72,59 +87,61 @@ def basic_ann_lsh(X, k, m, blocksz, n_core, edge_arr, w_arr):
   return repedge_arr, repw_arr
 
 def refine(X, edge_arr, w_arr):
-  edge_arr = edge_arr.astype(int)
-  ref_edge_arr = edge_arr
-  ref_w_arr = w_arr
+  n = edge_arr.shape[0]
   k = edge_arr.shape[1]
+  edge_arr = edge_arr.astype(int)
+  in_edge_arr = edge_arr.astype(int)
+  chunks = chunks(range(n), 20000)
   for j in range(k):
-    ref_edge_arr = np.hstack((ref_edge_arr, edge_arr[edge_arr[:,j], :]))
-    unidx = list(map(unique_idx, ref_edge_arr))
-    refined = (np.setdiff1d( ref_edge_arr[row, unidx[row]], row) for row in range(len(unidx)))
-    i = 0
-    for i_edge_list in refined:
-      iw = np.sqrt(np.square(X[i,:] - X[i_edge_list,:]).sum(1))
-      ref_w_arr[i,:] = np.sort(iw)[0:k]
-      ref_edge_arr[i,0:k] = i_edge_list[np.argsort(iw)[0:k]]
-      i += 1
-    
-    ref_edge_arr = ref_edge_arr[:,0:k]
+    for chunk in chunks:
+      ref_edge_arr = in_edge_arr[in_edge_arr[chunk,j], :]
+      stack_edge_arr = np.hstack((edge_arr[chunk,:], ref_edge_arr))
+      X_NNchunk = X[ref_edge_arr, :]
+      ref_w_arr = np.sqrt(np.square(X[chunk,np.newaxis,:] - X_NNchunk).sum(2))
+      stack_w_arr = np.hstack((w_arr[chunk,:], ref_w_arr))
+      unidx = [unique_idx(stack_edge_arr[i,:]) for i in range(len(chunk))]
+      unidx = [unidx[i][stack_edge_arr[i, unidx[i]] != chunk[i]] for i in range(len(chunk))]
+      sortidx = [np.argsort(stack_w_arr[i, unidx[i]])[range(k)] for i in range(len(chunk))]
+      edge_arr[chunk,:] = np.stack([stack_edge_arr[i, unidx[i]][sortidx[i]] for i in range(len(chunk))])
+      w_arr[chunk,:] = np.stack([stack_w_arr[i, unidx[i]][sortidx[i]] for i in range(len(chunk))])
   
-  return ref_edge_arr, ref_w_arr  
-
+  return edge_arr, w_arr
 
 def chunks(lst, n):
   for i in range(0, len(lst), n):
     yield lst[i:i+n]
 
+def skip_diag_strided(A):
+  m = A.shape[0]
+  strided = np.lib.stride_tricks.as_strided
+  s0,s1 = A.strides
+  return strided(A.ravel()[1:], shape=(m-1,m), strides=(s0+s1,s1)).reshape(m,-1)
+
 def distance_matrix(A):
-  return np.sqrt(np.square(A[np.newaxis,:,:]-A[:,np.newaxis,:]).sum(2))
+  M = np.sqrt(np.square(A[np.newaxis,:,:]-A[:,np.newaxis,:]).sum(2))
+  M = skip_diag_strided(M)
+  return M
 
 def unique_idx(row):
   return np.unique(row, return_index = True)[1]
 
+def idx_unique(a):
+  weight = 1j*np.linspace(0, a.shape[1], a.shape[0], endpoint=False)
+  b = a + weight[:, np.newaxis]
+  u, ind = np.unique(b, return_index=True)
+  b = np.zeros_like(a)
+  np.put(b, ind, np.ones(shape = a.shape).flat[ind])
+  return b.astype(bool)
+
 def brute_force_knn_par(A, idx, k, blocksz, edge_mat, w_mat):
   mat = distance_matrix(A)
   dismat = np.hstack((w_mat, mat))
-  idxmat = np.hstack((edge_mat, np.tile(idx, (len(idx), 1))   ))
-  outidx = np.ones(shape = (len(idx), k))*np.inf
-  outdis = np.ones(shape = (len(idx), k))*np.inf
-  for i in range(len(mat)):
-    vals, unidx = np.unique(idxmat[i,:], return_index = True)
-    unidx = list(unidx)
-    if i+k not in unidx:
-      print(idx)
-      print(i)
-      sys.stdout.flush()
-    unidx.remove([i+k])
-    outdis[i, 0:min(len(unidx)-1, k)] = np.sort(dismat[i,unidx])[0:min(len(unidx) - 1, k)]
-    min_k_indices = [unidx[i] for i in np.argsort(dismat[i,unidx])[0:min(len(unidx) -1, k)]]
-    outidx[i, 0:min(len(unidx)-1, k)] = idxmat[i,min_k_indices]
-    
+  tile_idx = skip_diag_strided(np.tile(idx, (len(idx), 1)))
+  idxmat = np.hstack((edge_mat,  tile_idx ))
+  sortdis = np.argsort(dismat, axis = -1)
+  dismat = np.sort(dismat, axis = -1)
+  idxmat = idxmat[np.arange(dismat.shape[0])[:,None], sortdis]
+  unidx = idx_unique(idxmat)
+  outdis = np.array([dismat[i, unidx[i]][0:k] for i in range(dismat.shape[0])])
+  outidx = np.array([idxmat[i, unidx[i]][0:k] for i in range(dismat.shape[0])])
   return [outidx, outdis]
-
-def func_star(a_b):
-  try:
-    return brute_force_knn_par(*a_b)
-  except Exception as e:
-    raise Exception(e)
-
